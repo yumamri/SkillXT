5340d8aedddfa65f8371fd84bdcce5d54d7f10db
yumamri <irma.houversing@gmail.com>
2020-12-10 19:14:20 +0100

    rearranged files
    clean

--- backend/src/main/java/ginb/SkillXT/persistence/entity/UserEntity.java
+++ backend/src/main/java/ginb/SkillXT/persistence/entity/UserEntity.java
@@ -3,15 +3,12 @@ package ginb.SkillXT.persistence.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "User");
+
 public class UserEntity {
+
     String email;
     String name;
     String family;
-    int age;
-    String gender;
-
     public String getName() {
         return name;
     }
