a20a1a819b5bb7d46ae7494753dd7025b228fa9e
yumamri <irma.houversing@gmail.com>
2020-12-11 17:45:18 +0100

    linked to db and added properties to UserEntity

--- backend/src/main/java/ginb/SkillXT/persistence/entity/UserEntity.java
+++ backend/src/main/java/ginb/SkillXT/persistence/entity/UserEntity.java
@@ -1,14 +1,40 @@
 package ginb.SkillXT.persistence.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-
+@Entity
+@Table(name="user")
 public class UserEntity {
+    @Id
+            @Column(name = "id")
+            int id;
 
-    String email;
+    @Column(name = "name")
     String name;
+
+    @Column(name = "family")
     String family;
+
+    @Column(name = "email")
+    String email;
+
+    @Column(name = "password")
+    String password;
+
+    @Column(name = "country")
+    String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -23,5 +49,25 @@ public class UserEntity {
 
     public void setFamily(String family) {
         this.family = family;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
