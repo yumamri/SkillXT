6d44ac698fd9f99884ab21c946de545d1678e504
yumamri <irma.houversing@gmail.com>
2020-12-29 19:26:54 +0100

    update and renamed files

--- skillxt-service/src/main/resources/db/migration/V3__Create_Table_Skill.sql
+++ skillxt-service/src/main/resources/db/migration/V3__Create_Table_Skill.sql
@@ -3,14 +3,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `skill` (
     `id` int(11) NOT NULL,
-    `title` varchar(255) NOT NULL
+    `title` varchar(255) NOT NULL,
+    `type` int(11) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `skill`
     ADD PRIMARY KEY (`id`);
 ALTER TABLE `skill`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-INSERT INTO skill(`title`)
-VALUES ('Piano'), ('Cuisine'), ('Jardinage'), ('Dessin');
-
+ALTER TABLE `skill`
+    ADD FOREIGN KEY (`type`) REFERENCES skill_type(`id`);
---
+++ skillxt-service/src/main/resources/db/migration/V5__Insert_Skill_Type.sql
@@ -0,0 +1,39 @@
+INSERT INTO skill_type(`type`)
+VALUES
+    ('Finance'),
+    ('Bricolage'),
+    ('Lifestyle'),
+    ('Loisir'),
+    ('Musique'),
+    ('Langue');
+INSERT INTO skill(`title`,`type`)
+VALUES
+    ('Trading', 1),
+    ('Comptabilité', 1),
+    ('Budget', 1),
+    ('Immobilier', 1),
+    ('Plomberie', 2),
+    ('Electricité', 2),
+    ('Menuiserie', 2),
+    ('Charpentrie', 2),
+    ('Cuisine', 3),
+    ('Mode', 3),
+    ('DIY', 3)
+    ('Couture', 3),
+    ('Communication', 3),
+    ('Jardinage', 4),
+    ('Pêche', 4),
+    ('Peinture', 4),
+    ('Dessin', 4),
+    ('Piano', 5),
+    ('Violon', 5),
+    ('Guitare', 5),
+    ('DJ', 5),
+    ('Beatbox', 5),
+    ('Français', 6),
+    ('Anglais', 6),
+    ('Espagnol', 6),
+    ('Chinois', 6),
+    ('Coréen', 6),
+    ('Japonais', 6),
+    ('Khmer', 6);
