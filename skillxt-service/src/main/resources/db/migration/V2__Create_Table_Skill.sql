61e63116bf13eae4fe2255b12dfab739971f619a
yumamri <irma.houversing@gmail.com>
2020-12-29 21:24:25 +0100

    removed skill type

--- skillxt-service/src/main/resources/db/migration/V2__Create_Table_Skill.sql
+++ skillxt-service/src/main/resources/db/migration/V2__Create_Table_Skill.sql
@@ -4,12 +4,10 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `skill` (
     `id` int(11) NOT NULL,
     `title` varchar(255) NOT NULL,
-    `type` int(11) NOT NULL
+    `type` varchar(255) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `skill`
     ADD PRIMARY KEY (`id`);
 ALTER TABLE `skill`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `skill`
-    ADD FOREIGN KEY (`type`) REFERENCES skill_type(`id`);
