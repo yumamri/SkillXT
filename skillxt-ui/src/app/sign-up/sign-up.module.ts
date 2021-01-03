e1a929d59df2ac7ac5753b7364485f96bff86b48
yumamri <irma.houversing@gmail.com>
2021-01-03 17:22:03 +0100

    i fixed it

--- skillxt-ui/src/app/sign-up/sign-up.module.ts
+++ skillxt-ui/src/app/sign-up/sign-up.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { SignUpPageRoutingModule} from "./sign-up-routing.module";
 import { SignUpPage} from './sign-up.page';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -11,6 +12,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
+      SignUpPageRoutingModule
   ],
   declarations: [SignUpPage]
 })
