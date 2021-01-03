66bd6d43865e94928869b1a6a6ca3939fdc277c9
yumamri <irma.houversing@gmail.com>
2021-01-03 14:04:08 +0100

    we can now search the skills

--- skillxt-ui/src/app/sign-up/sign-up.module.ts
+++ skillxt-ui/src/app/sign-up/sign-up.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { SignUpPageRoutingModule } from './sign-up-routing.module';
 import { SignUpPage} from './sign-up.page';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -12,7 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    SignUpPageRoutingModule
   ],
   declarations: [SignUpPage]
 })
